00c14461238dec5c0a28758edfb8de360950fc8d
Ephraim Arizona <degreatarizona@gmail.com>
2024-11-29 17:55:48 +0100

    Update knowledge.js

--- knowledge.js
+++ knowledge.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://task-manager-murex-six.vercel.app/api";
 fetchTasks();
 async function signup() {
     const name = document.getElementById('signupName').value;
